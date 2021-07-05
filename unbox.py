bdd9256c41331843d1ef3b9846572086515901a3
Squirrel23 <86971945+Squirrel23@users.noreply.github.com>
2021-07-05 20:29:20 +0530

    Add files via upload

---
+++ unbox.py
@@ -0,0 +1,30 @@
+import os
+os.system("cls")
+
+print(-14//4)
+
+print()
+## refer notebook CS
+print(-(2%10))
+print(-2%10)
+print((-2)%(-10))
+print(2%10)
+print(2%(-10))
+
+
+print()
+int = 10.5
+float = 10
+print(int)
+print(float)
+
+print()
+print(print("hello"))
+
+print()
+print(2//10)
+print(-2//10)
+print(-2//-10)
+
+
+
